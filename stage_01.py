2320eccb134b95faefd897b7cee99961bd40691f
Anant Mohan <anant4830@gmail.com>
2024-11-14 19:42:50 +0530

    data versioning and reproducibiltu using DVC added

---
+++ stage_01.py
@@ -0,0 +1,5 @@
+with open("artifact.txt", "w") as f:
+    f.write("my name is Anant and here i am executing stage one")
+
+with open("artifact2.txt", "w") as f:
+    f.write("This is my second artifact file...!")
